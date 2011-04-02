e39bb0ddf93221333a24cce3036d7982975a1361
Peter Hultqvist <phq@endnode.se>
2011-04-02 12:13:50 +0200

    Manage errors in saver silently

--- HitProxy/Filters/Saver.cs
+++ HitProxy/Filters/Saver.cs
@@ -13,8 +13,8 @@ namespace HitProxy.Filters
 	/// </summary>
 	public class Saver : Filter
 	{
-		List<FileOutput> savings = new List<FileOutput>();
-		
+		List<FileOutput> savings = new List<FileOutput> ();
+
 		public override bool Apply (Request request)
 		{
 			if (request.Response == null)
@@ -24,11 +24,10 @@ namespace HitProxy.Filters
 			
 			//Intercept data connection
 			FileOutput save = new FileOutput (request, this);
-			lock (savings)
-			{
+			lock (savings) {
 				savings.Add (save);
 			}
-			request.Response.FilterData(save);
+			request.Response.FilterData (save);
 			
 			return true;
 		}
@@ -36,53 +35,59 @@ namespace HitProxy.Filters
 		public override Html Status (NameValueCollection httpGet, Request request)
 		{
 			Html html = Html.Format (@"<p>Saves request data with flag <strong>save</strong> onto disk.</p><ul>");
-			lock(savings)
-			{
-				foreach(FileOutput fo in savings)
-				{
-					html += Html.Format(@"<li>{0}</li>", fo);
+			lock (savings) {
+				foreach (FileOutput fo in savings) {
+					html += Html.Format (@"<li>{0}</li>", fo);
 				}
 			}
 			
-			return html + Html.Format("</ul>");
+			return html + Html.Format ("</ul>");
 		}
 
-		
+
 		class FileOutput : IDataFilter
 		{
 			FileStream file;
 			string path;
 			Saver saver;
-			
+
 			public FileOutput (Request request, Saver saver)
 			{
-				path = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments), "Downloads");
-				Directory.CreateDirectory (path);
-				DateTime now = DateTime.Now;
-				path = Path.Combine (path, now.ToShortDateString () + " " + now.ToLongTimeString() + " ");
-				path += request.Uri.Host + " " + new Random().Next(100) + " " + Path.GetFileName (request.Uri.AbsolutePath);
-				
-				file = new FileStream (path, FileMode.Create);
+				try {
+					path = Path.Combine (Environment.GetFolderPath (Environment.SpecialFolder.MyDocuments), "Downloads");
+					Directory.CreateDirectory (path);
+					DateTime now = DateTime.Now;
+					path = Path.Combine (path, now.ToShortDateString () + " " + now.ToLongTimeString () + " ");
+					path += request.Uri.Host + " " + new Random ().Next (100) + " " + Path.GetFileName (request.Uri.AbsolutePath);
+					
+					file = new FileStream (path, FileMode.Create);
+				} catch (Exception e) {
+					file.NullSafeDispose ();
+					file = null;
+				}
 				
 				this.saver = saver;
 			}
 
 			public void Send (byte[] inBuffer, int start, int inLength, IDataOutput output)
 			{
-				file.Write (inBuffer, start, inLength);
+				try {
+					if (file != null)
+						file.Write (inBuffer, start, inLength);
+				}
+				catch (Exception) {}
 				output.Send (inBuffer, start, inLength);
 			}
-			
+
 			public override string ToString ()
 			{
 				return path;
 			}
-			
+
 			public void Dispose ()
 			{
-				file.Close ();
-				lock (saver.savings)
-				{
+				file.NullSafeDispose ();
+				lock (saver.savings) {
 					saver.savings.Remove (this);
 				}
 			}
